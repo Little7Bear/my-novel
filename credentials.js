acb75f2b2e76ac24e218831ece18772debca2458
Little7Bear <2395373083@qq.com>
2020-03-29 13:01:39 +0800

    添加秘钥文件

---
+++ credentials.js
@@ -0,0 +1,12 @@
+module.exports = {
+    sessionSecret: '8oS2Cmg4HR',
+    passwordStr:'ZYxbv85gKp',
+    mongo: {
+        development: {
+            connectionString: 'mongodb://localhost:27017/crawler'
+        },
+        production: {
+            connectionString: 'mongodb://localhost:27017/crawler',
+        },
+    },
+};
